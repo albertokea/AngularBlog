3a5a7bd7524e0eae161b3e2c2b3466e43be14876
albertokea <atorrescota5@gmail.com>
2021-02-19 17:54:00 +0100

    Añadido bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,

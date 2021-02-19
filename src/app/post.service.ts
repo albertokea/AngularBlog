d003e40af22a9e3830e3e7276df135dd5d8f9d8d
albertokea <atorrescota5@gmail.com>
2021-02-19 18:43:16 +0100

    Creada interfaz Post en post.service.ts

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@angular/core';
 
+export interface Post {
+  title: string,
+  text: string,
+  author: string,
+  image: string,
+  date: string,
+  category: string,
+}
+
 @Injectable({
   providedIn: 'root'
 })

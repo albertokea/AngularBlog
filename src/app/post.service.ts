26cf62ef0ef700211715e7a72db6c7c9397aa6a2
albertokea <atorrescota5@gmail.com>
2021-02-20 19:50:06 +0100

    Añadidas funciones getAll, addPost y getPostbyCategory al servicio Posts

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -5,8 +5,8 @@ export interface Post {
   text: string,
   author: string,
   image: string,
-  date: string,
-  category?: string,
+  date: Date,
+  category: string,
 }
 
 @Injectable({
@@ -16,6 +16,29 @@ export class PostService {
 
   posts: Post[]
 
-  constructor() { }
+  constructor() {
+    this.posts = [
+      {
+        title: 'New covid research',
+        text: 'JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB, and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles. ',
+        author: 'User1',
+        image: 'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png',
+        date: new Date,
+        category: 'news'
+      }
+    ]
+  }
+
+  getAll() {
+    return this.posts;
+  }
+
+  addPost(post) {
+    return this.posts.push(post)
+  }
+
+  getPostbyCategory(category) {
+    return this.posts.filter(post => category === post.category)
+  }
 
 }

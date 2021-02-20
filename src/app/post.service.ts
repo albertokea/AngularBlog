ac21b760acbf62d09747b2d06e061e3b1024f032
albertokea <atorrescota5@gmail.com>
2021-02-20 20:35:58 +0100

    Post añadido al servicio

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -25,7 +25,15 @@ export class PostService {
         image: 'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png',
         date: new Date,
         category: 'news'
-      }
+      },
+      {
+        title: 'My travel to asia',
+        text: 'JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB, and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles. ',
+        author: 'User2',
+        image: 'https://dam.esquirelat.com/wp-content/uploads/2019/01/big-lebowski-jeff-bridges-regreso-cover-770x513.jpg',
+        date: new Date,
+        category: 'travel'
+      },
     ]
   }
 

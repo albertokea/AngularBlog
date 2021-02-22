0be7204d49ca968ebdc201be5aac97946e68d03a
albertokea <atorrescota5@gmail.com>
2021-02-22 20:28:11 +0100

    Los datos del array de Posts ahora se guardan el local storage

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -17,24 +17,29 @@ export class PostService {
   posts: Post[]
 
   constructor() {
-    this.posts = [
-      {
-        title: 'New covid research',
-        text: 'JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB, and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles. ',
-        author: 'User1',
-        image: 'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png',
-        date: new Date,
-        category: 'news'
-      },
-      {
-        title: 'My travel to asia',
-        text: 'JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB, and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles. ',
-        author: 'User2',
-        image: 'https://dam.esquirelat.com/wp-content/uploads/2019/01/big-lebowski-jeff-bridges-regreso-cover-770x513.jpg',
-        date: new Date,
-        category: 'travel'
-      },
-    ]
+
+    if (localStorage.getItem('posts_data')) {
+      this.posts = JSON.parse(localStorage.getItem('posts_data'));
+    } else {
+      this.posts = [
+        {
+          title: 'New covid research',
+          text: 'JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB, and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles. ',
+          author: 'User1',
+          image: 'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png',
+          date: new Date,
+          category: 'news'
+        },
+        {
+          title: 'My travel to asia',
+          text: 'JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB, and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles. ',
+          author: 'User2',
+          image: 'https://dam.esquirelat.com/wp-content/uploads/2019/01/big-lebowski-jeff-bridges-regreso-cover-770x513.jpg',
+          date: new Date,
+          category: 'travel'
+        },
+      ]
+    }
   }
 
   getAll() {
@@ -42,7 +47,8 @@ export class PostService {
   }
 
   addPost(post) {
-    return this.posts.unshift(post)
+    this.posts.unshift(post);
+    localStorage.setItem('posts_data', JSON.stringify(this.posts));
   }
 
   getPostbyCategory(category) {
